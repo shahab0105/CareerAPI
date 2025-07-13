a2a415f37b565f1d6c6381f7a763454811169b20
shahab irfan <shahabuddinirfan@gmail.com>
2025-07-13 17:25:45 +0500

    import prisma client because .prisma says its in /generated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'generated/prisma';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService){}
 
   create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+    return this.prisma.user.create({ data: createUserDto as User});
   }
 
   findAll() {
